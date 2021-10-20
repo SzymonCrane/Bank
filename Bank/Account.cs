54cdf7d8654d2c405604836dfcc0b41592250fae
Szymon Żuraw <szymon.zurawit@wp.pl>
2021-10-20 18:47:34 +0200

    Create classes Account, Actions, SavingsAccount, and interface IPrinter, create hierarchy.

---
+++ Bank/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public abstract class Account
+    {
+        public int Id { get; set; }
+        public long AccountNumber { get; set; }
+        public long Pesel { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Account(int id, long accountnumber, long pesel, string firstname, string lastname)
+    {
+            Id = id;
+            AccountNumber = accountnumber;
+            Pesel = pesel;
+            FirstName = firstname;
+            LastName = lastname;
+
+    }
+        public void Printer()
+        {
+            
+        }
+    }
+}
---
+++ Bank/Actions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public static class Actions
+    {
+        public static string GetFullName(string ClientFirstName, string ClientLastName)
+        {
+            return ClientFirstName + ClientLastName ;
+        }
+        public static long GetBalance(long oldBalance,long changeBalance)
+        {
+            return oldBalance + changeBalance;
+        }
+        public static int GenerateAccountNumber(int Id)
+        {
+            return AccountNumber;
+        }
+    }
+}
